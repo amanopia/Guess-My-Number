aa71beb238ba28aa5ddf083fcdb0fae7ed6dac9c
unknown <amanvermaspace@gmail.com>
2022-12-28 21:03:46 +0530

    added a check to check for empty input values

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const logger = function(){
     console.log("aPPLE");
 }
+
+let guess;
 document.querySelector('.check').addEventListener('click', function(){
-    console.log(document.querySelector('.guess').value);
+    guess = Number(document.querySelector('.guess').value);
+    
+    if(!guess){
+        // since empty input field means, 0, and zero is a falsy value, so !false true
+        document.querySelector('.message').textContent = "Enter a number";
+    }
+    console.log(guess, typeof guess);
 })
