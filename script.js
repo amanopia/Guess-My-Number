c7b0b231886b78c3ef1075cb273d4d2a0c997a15
unknown <amanvermaspace@gmail.com>
2022-12-29 08:24:27 +0530

    added the functionality to check if the score has been reduced to zero. If so, display : you lost the game

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let secretNumber = Math.ceil(Math.random() * 20);
 console.log(secretNumber);
 
 let scoreElement = document.querySelector('.score');
+let message = document.querySelector('.message');
 let score = 20;
 let highScoreElement = document.querySelector('.highscore');
 
@@ -18,16 +19,24 @@ document.querySelector('.check').addEventListener('click', function(){
     
     if(!guess){
         // since empty input field means, 0, and zero is a falsy value, so !false true
-        document.querySelector('.message').textContent = "Enter a number";
+        message.textContent = "Enter a number";
     } else if(guess === secretNumber) {
-        document.querySelector('.message').textContent = "Correct Number!!!";
+        message.textContent = "Correct Number!!!";
     } else if(guess > secretNumber) {
-        document.querySelector('.message').textContent = "Number too high!";
-        scoreReducer();
+        if(score > 0){
+            message.textContent = "Number too high!";
+            scoreReducer();
+        } else {
+            message.textContent = "You lost the game!";
+        }
         // since score is declared globally, we can use the function, since it is reducing the score on a global level
     } else {
-        document.querySelector('.message').textContent = "Number too low";
-        scoreReducer();
+        if(score > 0){
+            message.textContent = "Number too low";
+            scoreReducer();
+        } else {
+            message.textContent = "You lost the game!";
+        }
     }
     console.log(guess, typeof guess);
 })
